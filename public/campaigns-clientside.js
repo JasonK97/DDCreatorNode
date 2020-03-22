2822510b24ee8ca6530489ae5b40203cbe5b2b25
Jason Kent <jasonbkent@gmail.com>
2020-03-22 00:51:15 -0600

    header stuff

--- public/campaigns-clientside.js
+++ public/campaigns-clientside.js
@@ -72,3 +72,16 @@ function addCampaignToDb() {
         $("#addedCampaign").innerHTML("Successfully added" + data);
     });
 }
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("searchArea");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
